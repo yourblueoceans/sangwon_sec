6cae46c7cde3918e598bc15d94b4fabe6864aa30
yourblueoceans <yourblueoceans@gmail.com>
2025-12-29 19:10:00 +0900

    modified30

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // ★ 중요: 리포지토리 이름 앞뒤로 슬래시(/)를 꼭 붙여야 합니다.
   base: '/sangwon_sec/',
+  plugins: [react()],
 })
 
 
